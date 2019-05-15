09c66fe8148d28b10922e08158d2f8d4a3e5027b
cool <335814617@qq.com>
2019-05-15 17:39:12 +0800

    modify setup

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     version=read_version(),
     packages=find_packages(),
     description='This is a local tools for SCF.',
-    long_description=open('README.rst').read(),
+    long_description=open('README.md').read(),
     author='Tencent Cloud',
     url='https://github.com/tencentyun/tcfcli.git',
     maintainer_email="qcloud_middleware@qq.com",
