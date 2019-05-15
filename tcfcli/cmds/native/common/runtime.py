7b96c2c2ffd63297682397b559182a20fffdf8b9
NevenMoore <1083602539@qq.com>
2019-05-15 18:03:41 +0800

    bug fix

--- tcfcli/cmds/native/common/runtime.py
+++ tcfcli/cmds/native/common/runtime.py
@@ -12,7 +12,7 @@ class Runtime(object):
         self.codeuri = proper.get(CODE_URI)
         self.env = proper.get(ENVIRONMENT, {VARIABLE: {}}).get(VARIABLE, {})
         self.handler = proper.get(HANDLER, "")
-        self.mem_size = proper.get(CODE_URI, 128)
+        self.mem_size = proper.get(MEMORY_SIZE, 128)
         self.runtime = proper.get(RUNTIME, "").lower()
         self.timeout = proper.get(TIMEOUT, 3)
 
