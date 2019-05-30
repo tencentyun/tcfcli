52ffcf9ec5fa5a910e8893fc446805cd30fe2065
your_name <your_email>
2019-05-30 10:38:29 +0800

    bug fix

--- tcfcli/cmds/native/common/runtime/python2.7/pstool/__init__.py
+++ tcfcli/cmds/native/common/runtime/python2.7/pstool/__init__.py
@@ -9,7 +9,7 @@ __all__ = ['get_peak_memory']
 def get_peak_memory():
     try:
         if sysstr.lower() == "windows":
-            from winps import win_peak_memory
+            from .winps import win_peak_memory
             return win_peak_memory()
         import resource
 
@@ -17,6 +17,7 @@ def get_peak_memory():
             return int(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / (2**20))
         return int(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / (2**10))
     except Exception as err:
+        print("get memory info failed, {}".format(str(err)))
         return 0
 
 
--- tcfcli/cmds/native/common/runtime/python2.7/pstool/winps.py
+++ tcfcli/cmds/native/common/runtime/python2.7/pstool/winps.py
@@ -11,7 +11,17 @@ SIZE_T = ctypes.c_size_t
 
 class PROCESS_MEMORY_COUNTERS_EX(ctypes.Structure):
     _fields_ = [
+        ('cb', wintypes.DWORD),
+        ('PageFaultCount', wintypes.DWORD),
         ('PeakWorkingSetSize', SIZE_T),
+        ('WorkingSetSize', SIZE_T),
+        ('QuotaPeakPagedPoolUsage', SIZE_T),
+        ('QuotaPagedPoolUsage', SIZE_T),
+        ('QuotaPeakNonPagedPoolUsage', SIZE_T),
+        ('QuotaNonPagedPoolUsage', SIZE_T),
+        ('PagefileUsage', SIZE_T),
+        ('PeakPagefileUsage', SIZE_T),
+        ('PrivateUsage', SIZE_T),
     ]
 
 GetProcessMemoryInfo = ctypes.windll.psapi.GetProcessMemoryInfo
@@ -36,4 +46,4 @@ def win_peak_memory(process=None):
     if not ret:
         return 0
     info = dict((name, getattr(counters, name)) for name, _ in counters._fields_)
-    return info["PeakWorkingSetSize"] / (2**20)
+    return int(info["PeakWorkingSetSize"] / (2**20))
